fafa4d392c5cd3f743d1b8b129e0e3ae1cb8daa4
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-07-07 15:28:43 +0200

    Calculator source fixed

--- js/calculator.js
+++ js/calculator.js
@@ -3,12 +3,10 @@ var Calculator = function (initial) {
   this.record = [];
 };
 
-Calculator.prototype.add = function (random) {
-  random = random || false;
-
+Calculator.prototype.add = function () {
   for(var i = 0; i < arguments.length; i++) {
     this.value += arguments[i];
-    this.record.push({method: 'add', value: arguments[i], random: random})
+    this.record.push({method: 'add', value: arguments[i]})
   }
 
   return this.value;
@@ -30,7 +28,7 @@ Calculator.prototype.addRandom = function () {
 
   var num = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  return this.add(num, true);
+  return this.add(num);
 };
 
 Calculator.prototype.bigger = function (value) {
