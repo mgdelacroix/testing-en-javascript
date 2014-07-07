49cffb3edceb3d1d6b68420d64bea41acab9b3ca
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-07-07 14:38:35 +0200

    Calculator specs

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -1,16 +1,33 @@
 describe('calculadora', function() {
     beforeEach(function() {
-
+        this.calculadora = new Calculator();
     })
 
     afterEach(function() {
-
     })
 
     it('puedo sumar', function(){
-        var calculadora = new Calculator();
+        expect(this.calculadora.add(2, 3)).to.be.equal(5);
+        expect(this.calculadora.add(7)).to.be.equal(12);
+    })
 
-        expect(calculadora.add(2, 3)).to.be.equal(5);
+    it('puedo restar', function() {
+        expect(this.calculadora.sub(1)).to.be.equal(-1);
+        expect(this.calculadora.sub(20, 30)).to.be.equal(-51);
+    })
+
+    it('puedo comprobar que un número sea mayor que otro', function() {
+        this.calculadora.value = 50;
+
+        expect(this.calculadora.bigger(100)).to.be.true;
+        expect(this.calculadora.bigger(10)).not.to.be.true;
+    })
+
+    it('puedo comprobar que un número sea menor que otro', function() {
+        this.calculadora.value = 50;
+
+        expect(this.calculadora.smaller(100)).not.to.be.true;
+        expect(this.calculadora.smaller(10)).to.be.true;
     })
 
 })
