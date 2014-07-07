4cc2c2b4d23b43be3bab9d221257ce81fdd60ae9
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-07-07 15:36:34 +0200

    semicolons added

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -1,20 +1,20 @@
 describe('calculadora', function() {
     beforeEach(function() {
         this.calculadora = new Calculator();
-    })
+    });
 
     afterEach(function() {
-    })
+    });
 
     it('puedo sumar', function(){
         expect(this.calculadora.add(2, 3)).to.be.equal(5);
         expect(this.calculadora.add(7)).to.be.equal(12);
-    })
+    });
 
     it('puedo restar', function() {
         expect(this.calculadora.sub(1)).to.be.equal(-1);
         expect(this.calculadora.sub(20, 30)).to.be.equal(-51);
-    })
+    });
 
     it('puedo comprobar que un número sea mayor que otro', function() {
         this.calculadora.value = 50;
@@ -22,14 +22,14 @@ describe('calculadora', function() {
         expect(this.calculadora.bigger(100)).to.be.true;
         expect(this.calculadora.bigger(10)).not.to.be.true;
         expect(this.calculadora.bigger(-10)).to.be.false;
-    })
+    });
 
     it('puedo comprobar que un número sea menor que otro', function() {
         this.calculadora.value = 50;
 
         expect(this.calculadora.smaller(100)).not.to.be.true;
         expect(this.calculadora.smaller(10)).to.be.true;
-    })
+    });
 
     it('puedo revisar el histórico', function() {
         this.calculadora.add(30, 4, 60);
@@ -46,6 +46,6 @@ describe('calculadora', function() {
         console.log(this.calculadora.record);
 
         expect(this.calculadora.record).to.be.eql(historico);
-    })
+    });
 
-})
+});
