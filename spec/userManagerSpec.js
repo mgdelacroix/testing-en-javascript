88c226c2262986feefb629fe33a26f046ee7d783
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-07-07 16:24:44 +0200

    add user test

---
+++ spec/userManagerSpec.js
@@ -0,0 +1,24 @@
+describe('gestor de usuarios', function() {
+    beforeEach(function() {
+        this.userManager = new UserManager();
+    });
+
+    afterEach(function() {
+
+    });
+
+    it('puedo añadir usuarios', function() {
+        var startWithStub = sinon.stub(Validator, "startWith");
+        startWithStub.withArgs("alonso", "a").returns(true);
+        startWithStub.withArgs("jimmy", "a").returns(false);
+
+        this.userManager.add("alonso");
+        this.userManager.add("jimmy");
+        var userList = this.userManager.get();
+        var expectedUserList = [ 'jimmy' ]
+
+        expect(userList).to.be.eql(expectedUserList);
+
+        startWithStub.restore();
+    });
+});
